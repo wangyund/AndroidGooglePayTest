34e90f0799bdf6f4cabc75cb2e6d86f039049339
wangyund <wuyezhiguhun@163.com>
2021-05-06 09:41:15 +0800

    不加固。

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#  0. 初始化环境变量
-
 pwd=$PWD
 
 export rootPwd=${pwd}
@@ -12,16 +10,14 @@ echo "目录结构如下所示："
 ls -l
 
 ## 2.1 SDK代码混淆以及变量替换
-
-
 cd ${pwd}
 
 #开始打包
 ./gradlew assembleRelease
 
 # 打包后的处理
-chmod +x ${rootPwd}/scripts/07_final_deal.sh
-${rootPwd}/scripts/07_final_deal.sh
+#chmod +x ${rootPwd}/scripts/07_final_deal.sh
+#${rootPwd}/scripts/07_final_deal.sh
 
 
 
